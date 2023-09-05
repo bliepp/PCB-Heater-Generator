5e4cdf409cece62054f2a8a574bc9e17a2239d07
bliepp <12013202+bliepp@users.noreply.github.com>
2023-09-05 23:23:16 +0200

    Present calculated values nicely to user

--- main.py
+++ main.py
@@ -42,9 +42,13 @@ n, track_length, resistance, current, pcb_width = trace.generate_serpentine(
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Power", f"{Float(voltage*current):!.1h}W", f"Max: {Float(max_current*voltage):!.1h}W", delta_color="off")
 col2.metric("Electrical Resistance", f"{Float(resistance):!.2h}Ω", f"Min: {Float(voltage/max_current):!.2h}Ω", delta_color="off")
-col3.metric("Track Length", f"{track_length:.1f} mm", f"Min: {minimal_track_length:.1f} mm", delta_color="off")
-col4.metric("Trace Width", f"{trace.width:.4f} mm")
+col3.metric("Approx. Current", f"{Float(current):!.2h}A")
+col4.metric("Number of serpentines", n)
 
-st.write(pcb_width, current, n)
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Track Length", f"{track_length:.1f} mm", f"Min: {minimal_track_length:.1f} mm", delta_color="off")
+col2.metric("Trace Width", f"{trace.width:.4f} mm")
+col3.metric("PCB Width", f"{pcb_width:.1f} mm")
+col4.metric("PCB Height", f"{pcb_height:.1f} mm")
 
 st.download_button("Download KiCAD Footprint", bytes(), "footprint.kicad_mod")
